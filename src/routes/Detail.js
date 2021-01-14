b6e71a763bb04ba216e5cb2899a7b042ab4a9083
june6723 <june6723@naver.com>
2021-01-14 14:04:44 +0900

    Movie

--- src/routes/Detail.js
+++ src/routes/Detail.js
@@ -1,31 +1,78 @@
 import React from "react";
 import { useParams } from "react-router-dom";
 import { gql, useQuery } from "@apollo/client";
+import styled from "styled-components";
 
 const GET_MOVIE = gql`
-  #   query getMovie($id: Int!) {
-  {
+  query getMovie($id: Int!) {
     movie(id: $id) {
-      id
       title
+      language
+      rating
       medium_cover_image
       description_intro
     }
   }
 `;
+const Container = styled.div`
+  height: 100vh;
+  background-image: linear-gradient(-45deg, #d754ab, #fd723a);
+  width: 100%;
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+  color: white;
+`;
+
+const Column = styled.div`
+  width: 50%;
+  margin-left: 10px;
+`;
+
+const Title = styled.h1`
+  font-size: 48px;
+  margin-bottom: 15px;
+`;
+
+const Subtitle = styled.h4`
+  font-size: 26px;
+  margin-bottom: 10px;
+`;
+
+const Description = styled.p`
+  font-size: 20px;
+`;
+
+const Poster = styled.div`
+  width: 25%;
+  height: 60%;
+  background-image: url(${(props) => props.bg});
+  background-color: transparent;
+  background-size: cover;
+  background-position: center center;
+`;
 
 const Detail = () => {
   const { id } = useParams();
-  const parsedID = Number.parseInt(id);
-  console.log(parsedID);
-  const { loading, data } = useQuery(GET_MOVIE, { variables: { parsedID } });
-  console.log(data);
-  if (loading) {
-    return <span>"loading"</span>;
-  }
-  if (data && data.movie) {
-    return <span>data.movie.title</span>;
-  }
+  const { loading, data } = useQuery(GET_MOVIE, { variables: { id: +id } });
+  return (
+    <Container>
+      <Column>
+        <Title>{loading ? "Loading..." : data.movie.title}</Title>
+        {!loading && data.movie && (
+          <>
+            <Subtitle>
+              {data.movie.language} · {data.movie.rating}
+            </Subtitle>
+            <Description>{data.movie.description_intro} </Description>
+          </>
+        )}
+      </Column>
+      <Poster
+        bg={data && data.movie ? data.movie.medium_cover_image : ""}
+      ></Poster>
+    </Container>
+  );
 };
 
 export default Detail;
--- src/routes/Home.js
+++ src/routes/Home.js
@@ -42,6 +42,14 @@ const Loading = styled.div`
   font-weight: 500;
   margin-top: 10px;
 `;
+const Movies = styled.div`
+  display: grid;
+  grid-template-columns: repeat(4, 1fr);
+  grid-gap: 25px;
+  width: 60%;
+  position: relative;
+  top: -50px;
+`;
 
 const Home = () => {
   const { loading, data } = useQuery(GET_MOVIES);
@@ -52,9 +60,13 @@ const Home = () => {
         <Subtitle>I love GraphQL</Subtitle>
       </Header>
       {loading && <Loading>Loading...</Loading>}
-      {!loading &&
-        data.movies &&
-        data.movies.map((m) => <Movie key={m.id} id={m.id} />)}
+      {!loading && data.movies && (
+        <Movies>
+          {data.movies.map((m) => (
+            <Movie key={m.id} id={m.id} bg={m.medium_cover_image} />
+          ))}
+        </Movies>
+      )}
     </Container>
   );
 };
